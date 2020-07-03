39aaaffd54bf3d03270187ac428cf2e981974eb8
Catalina Williams <catalina.rose1361@gmail.com>
2020-07-03 17:06:35 -0600

    footer and additional styling for table

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -78,11 +78,21 @@ function populateChart() {
         {
           label: "Total Over Time",
           fill: true,
-          backgroundColor: "#6666ff",
+          backgroundColor: "#00c853 ",
           data
         }
       ]
-    }
+    },
+    options: {
+      layout: {
+          padding: {
+              left: 50,
+              right: 50,
+              top: 50,
+              bottom: 50
+          }
+      }
+  }
   });
 }
 
